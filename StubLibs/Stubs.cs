f072cc0b2309a046d7fdb6e17c54bffd6b5bde85
ferrles124 <mehmetemredursun9@gmail.com>
2026-04-17 21:46:36 +0300

    Update Stubs.cs

--- StubLibs/Stubs.cs
+++ StubLibs/Stubs.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Xna.Framework {
         public static Color White;
         public static Color Black;
         public static Color Gray;
+        public static Color Red;
         public Color(int r, int g, int b, int a = 255) { }
     }
     public struct Rectangle {
@@ -36,17 +37,20 @@ namespace Microsoft.Xna.Framework {
 
 namespace Microsoft.Xna.Framework.Graphics {
     public class Texture2D { public int Width; public int Height; }
-    public class SpriteFont { }
+    public class SpriteFont { public Microsoft.Xna.Framework.Vector2 MeasureString(string s) => new(); }
     public class SpriteBatch {
         public void Draw(Texture2D t, Microsoft.Xna.Framework.Rectangle r, Microsoft.Xna.Framework.Color c) { }
         public void DrawString(SpriteFont f, string s, Microsoft.Xna.Framework.Vector2 pos, Microsoft.Xna.Framework.Color c) { }
+        public void Begin() { }
+        public void End() { }
     }
     public class GraphicsDevice { }
+    public class RenderTarget2D : Texture2D { }
 }
 
 namespace Microsoft.Xna.Framework.Input {
     public struct KeyboardState { }
-    public enum Keys { Enter, Escape, Back, Tab }
+    public enum Keys { Enter, Escape, Back, Tab, A, C, V, X, Z, Y }
 }
 
 namespace Microsoft.Xna.Framework.Content {
@@ -55,9 +59,13 @@ namespace Microsoft.Xna.Framework.Content {
 
 namespace StardewValley {
     public enum Season { Spring, Summer, Fall, Winter }
+    public enum Gender { Male, Female, Undefined }
     public class WorldDate {
         public int Year; public string Season; public int DayOfMonth;
         public WorldDate(int year, string season, int day) { }
+    }
+    public class LocalizedContentManager {
+        public enum LanguageCode { en, de, es, fr, hu, it, ja, ko, pt, ru, tr, zh }
     }
     public class LanguageCode { }
     public class DialogueLine {
@@ -72,19 +80,35 @@ namespace StardewValley {
         public MarriageDialogueReference(string file, string key, bool gendered = false, params string[] args) { }
         public Dialogue GetDialogue(NPC n) => null;
     }
+    public class Friendship {
+        public int Points;
+        public bool IsMarried() => false;
+        public bool IsDating() => false;
+        public bool IsEngaged() => false;
+    }
+    public class Crop { public string indexOfHarvest; }
+    public class FarmAnimal { public string displayName; public string type; }
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue> { }
     public class Game1 {
         public static Farmer player;
         public static List<GameLocation> locations = new();
         public static GameLocation currentLocation;
         public static string currentSeason;
         public static int pixelZoom = 4;
+        public static Microsoft.Xna.Framework.Graphics.SpriteFont dialogueFont;
+        public static Microsoft.Xna.Framework.Graphics.Texture2D mouseCursors;
         public static void drawDialogue(NPC n) { }
         public static void DrawDialogue(NPC n) { }
+        public static void activeClickableMenu_setter(Menus.IClickableMenu menu) { }
+        public static Menus.IClickableMenu activeClickableMenu;
     }
     public class Farmer {
         public string Name;
         public GameLocation currentLocation;
+        public Gender gender;
+        public Dictionary<string, Friendship> friendshipData = new();
         public List<Characters.Child> getChildren() => new();
+        public int getFriendshipHeartLevelForNPC(string name) => 0;
     }
     public class GameLocation {
         public string Name;
@@ -94,6 +118,8 @@ namespace StardewValley {
     public class NPC : Character {
         public string Name;
         public Dialogue CurrentDialogue;
+        public Gender gender;
+        public int Age;
         public void setNewDialogue(string text, bool add = false, bool clear = false) { }
         public Microsoft.Xna.Framework.Vector2 getTileLocation() => new();
         public virtual void checkAction(Farmer f, GameLocation l) { }
@@ -101,19 +127,26 @@ namespace StardewValley {
         public virtual void PushTemporaryDialogue(Dialogue d) { }
         public virtual Dialogue TryGetDialogue(string key) => null;
         public virtual Dialogue TryToRetrieveDialogue(string key) => null;
+        public virtual Dialogue tryToRetrieveDialogue(string key) => null;
         public virtual Dialogue TryToGetMarriageSpecificDialogue(string key) => null;
+        public virtual Dialogue tryToGetMarriageSpecificDialogue(string key) => null;
         public virtual void CheckForNewCurrentDialogue(int hearts, bool onlyCheck = false) { }
+        public virtual void checkForNewCurrentDialogue(int hearts, bool onlyCheck = false) { }
         public virtual string GetGiftReaction(Object gift) => "";
+        public bool isMarried() => false;
+        public bool isDivorcedFrom(Farmer f) => false;
+        public string getTermOfSpousalEndearment() => "";
     }
     public class Character { }
     public class Item { }
-    public class Object : Item { public string Name; public string Category; }
+    public class Object : Item { public string Name; public string Category; public string DisplayName; }
     public class Dialogue {
         public Dialogue(NPC speaker, string dialogueText) { }
         public Dialogue(NPC speaker, string file, string key) { }
         public NPC speaker;
         public void Clear() { }
         public virtual string chooseResponse(List<Response> responses) => "";
+        public virtual Dialogue TryGetDialogue(string key) => null;
     }
 }
 
@@ -126,10 +159,14 @@ namespace StardewValley.Menus {
         public virtual void draw(Microsoft.Xna.Framework.Graphics.SpriteBatch b) { }
         public virtual void receiveLeftClick(int x, int y, bool playSound = true) { }
         public virtual void receiveRightClick(int x, int y, bool playSound = true) { }
+        public virtual void receiveKeyPress(Microsoft.Xna.Framework.Input.Keys key) { }
         public virtual void update(Microsoft.Xna.Framework.GameTime time) { }
         public virtual void gameWindowSizeChanged(Microsoft.Xna.Framework.Rectangle oldBounds, Microsoft.Xna.Framework.Rectangle newBounds) { }
         public virtual void emergencyShutDown() { }
+        public virtual void cleanupBeforeExit() { }
+        public virtual bool overrideSnappyMenuCursorMovementBan() => false;
         public bool destroy;
+        public int xPositionOnScreen, yPositionOnScreen, width, height;
     }
     public class ClickableTextureComponent {
         public Microsoft.Xna.Framework.Rectangle bounds;
@@ -154,9 +191,23 @@ namespace StardewValley.Characters {
 }
 
 namespace StardewValley.GameData {
+    public class CharacterData { public string DisplayName; public string Age; public string Gender; }
+    public class HomeRenovation { }
+}
+
+namespace StardewValley.GameData.Characters {
     public class CharacterData { }
 }
 
 namespace StardewValley.Objects {
     public class Hat : StardewValley.Object { }
+    public class Trinket : StardewValley.Object { }
+}
+
+namespace StardewValley.Objects.Trinkets {
+    public class Trinket : StardewValley.Object { }
+}
+
+namespace StardewValley.Buildings {
+    public class Building { public string buildingType; }
 }
